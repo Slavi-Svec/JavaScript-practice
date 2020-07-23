a15710d62a50cd27f1ec1adf23e787bc5eb55ca9
izaak svec <slavi.svec@gmail.com>
2020-07-24 07:53:21 +1000

    updated image button

--- Image machine/script.js
+++ Image machine/script.js
@@ -1,6 +1,4 @@
-const btn = document.getElementsByClassName ('btn')[0]
+const btn = document.getElementsByClassName('btn')[0]
 btn.addEventListener('click', () => {
-console.log('hello')
+  document.getElementsByClassName("image")[0].style.backgroundImage = "url(https://picsum.photos/200/300/?random&t=" + new Date().getTime() +")"
 })
-
-
