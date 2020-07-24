755e5650d33bbcbeaa44f70e180d1c7cf1f95377
izaak svec <slavi.svec@gmail.com>
2020-07-24 10:18:10 +1000

    Updated time functions JS

--- Clock/script.js
+++ Clock/script.js
@@ -1,6 +1,12 @@
-const btn = document.getElementsByClassName ('btn')[0]
-btn.addEventListener('click', () => {
-console.log('hello')
-})
+const screen = document.getElementsByClassName ('clock-screen')[0]
 
+const newTime = ()  => {
+  const d = new Date()
+  const s = d.getSeconds()
+  const m = d.getMinutes()
+  const h = d.getHours()
+  screen.textContent = h + ':' + m + ':' + s
+  }
+
+  console.log( setInterval(newTime, 1000))
 
