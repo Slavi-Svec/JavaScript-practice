b26d17aff3bc682b04b8621a207cb3c474fb2530
izaak svec <slavi.svec@gmail.com>
2020-07-26 19:11:36 +1000

    Updated palindrome function

--- small projects /Palindrome checker/script.js
+++ small projects /Palindrome checker/script.js
@@ -2,13 +2,13 @@ const value = document.getElementsByClassName('palindrome-in')[0].value
 const textOut = document.getElementsByClassName('palindrome-out')[0]
 const btn = document.getElementsByClassName('btn')[0]
 
-function palindrome(str) {
+const palindrome = (str) => {
    str = str.replace(/\W/g, '').toLowerCase();
    return str === str.split('').reverse().join('');
 }
 
 btn.addEventListener('click', () => {
-  'use strict';
+
   if (palindrome(value)) {
     textOut.innerHTML = "yes yes yes... you got a palindrome"
   } else {
