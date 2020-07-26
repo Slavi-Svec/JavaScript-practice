b797c0b75c26d5e73cef87ecfa79049962d3a69f
izaak svec <slavi.svec@gmail.com>
2020-07-27 07:41:06 +1000

    Updates weather app preactice

---
+++ Weather apps/weather app/script.js
@@ -0,0 +1,27 @@
+// const btn = document.getElementsByClassName('btn')[0]
+const seeLocation = document.getElementsByClassName('location')[0]
+const seeWeather = document.getElementsByClassName('weather-out')[0]
+
+  const updateWeather = () => {
+    const fetchTheUrl =  'http://api.openweathermap.org/data/2.5/weather?zip=2042,au&units=metric&appid=e1ec3878f6bdc360d0bd74911e0ba7fa'
+    fetch(fetchTheUrl)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        const getLocation = data.name
+        seeLocation.textContent = getLocation
+        const getTemp = data.main.temp
+        seeWeather.textContent = getTemp
+      })
+    }
+
+    window.onload = function () {
+      updateWeather()
+      setInterval(function () {
+        updateWeather()
+        }, 300)
+    }
+
+
+    console.log(updateWeather())
