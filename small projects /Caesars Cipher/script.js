883a60e10ea45870f31cc934349205d1bc081899
izaak svec <slavi.svec@gmail.com>
2020-08-08 06:56:12 +1000

    Updated styles

--- small projects /Caesars Cipher/script.js
+++ small projects /Caesars Cipher/script.js
@@ -1,22 +1,20 @@
-function rot13(str) {
-  const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  return str
-    .split('')
-    .map(char => {
-      const pos = alphabet.indexOf(char);
-      return pos >= 0 ? alphabet[(pos + 13) % 26] : char;
-    })
-    .join('');
+const rot13 = (str) => {
+  let decoded = ""
+  const charCheck = (char) => char < 65 ? char : char % 26 + 65
+  for(let i = 0; i < str.length; i++) {
+    var x = str.charCodeAt(i)
+    decoded += String.fromCharCode(charCheck(x))
+  }
+  return decoded
 }
 
-  console.log(rot13(''))
+console.log(rot13('serr'))
 
-function decodeRot13(){
+const decodeRot13 = () => {
   let input = document.getElementsByClassName('word-in')[0]
   let result = document.getElementsByClassName("word-out")[0]
   result.textContent = rot13(input.value)
 }
-
 
 
 
