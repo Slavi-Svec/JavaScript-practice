b6c5f2a91720e760fb2a7b719e5a4bb8a02a64c7
izaak svec <slavi.svec@gmail.com>
2020-08-10 18:16:15 +1000

    completed cash register JS

--- small projects /Cash Register/script.js
+++ small projects /Cash Register/script.js
@@ -1,15 +1,64 @@
-const msgIn = document.getElementsByClassName('message-in')[0]
-const msgOut = document.getElementsByClassName('message-out')[0]
-const btn = document.getElementsByClassName('btn')[0]
+const  checkCashRegister = (price, cash, cid) => {
+  const currency = [
+    { name: 'ONE HUNDRED', val: 100.0 },
+    { name: 'TWENTY', val: 20.0 },
+    { name: 'TEN', val: 10.0 },
+    { name: 'FIVE', val: 5.0 },
+    { name: 'ONE', val: 1.0 },
+    { name: 'QUARTER', val: 0.25 },
+    { name: 'DIME', val: 0.1 },
+    { name: 'NICKEL', val: 0.05 },
+    { name: 'PENNY', val: 0.01 }
+  ]
 
-btn.addEventListener('click', () => {
-    let msgContent = msgIn.value
+ const out = { change: [], status: null }
+ let change = cash - price
 
-    if (msgContent === '') {
-        alert('please Enter A Message')
-      } else {
-        msgOut.innerHTML = msgContent
-        msgIn.value = ''
+  let register = cid.reduce(
+    function(acc, curr) {
+      acc.total += curr[1]
+      acc[curr[0]] = curr[1]
+      return acc
+    },
+    { total: 0 }
+  );
+
+  if (register.total === change) {
+    out.status = 'CLOSED'
+    out.change = cid
+    return out
+  }
+
+  if (register.total < change) {
+    out.status = 'INSUFFICIENT_FUNDS'
+    return out
+  }
+
+  var change_arr = currency.reduce(function(acc, curr) {
+    var value = 0
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val
+      register[curr.name] -= curr.val
+      value += curr.val
+
+      change = Math.round(change * 100) / 100
     }
-})
+    if (value > 0) {
+      acc.push([curr.name, value])
+    }
+    return acc
+  }, [])
 
+
+  if (change_arr.length < 1 || change > 0) {
+    out.status = "INSUFFICIENT_FUNDS"
+    return out
+  }
+
+  out.status = "OPEN"
+  out.change = change_arr
+  return out
+}
+
+
+console.log(checkCashRegister(19.5, 20,))
