b85a5346f5d66bb1f89825523369409578b9c993
izaak svec <slavi.svec@gmail.com>
2020-08-19 09:13:29 +1000

    Updated solution

--- codewars/code.js
+++ codewars/code.js
@@ -1,8 +1,1 @@
-function greet(name){
-  return "Hello,"+' ' + name + ' '+"how are you doing today?"
-}
-
-// greet = (name) =>  "Hello,"+' ' + name + ' '+"how are you doing today?"
-
-
-
+greet = (name) =>  `Hello, ${name} how are you doing today?`
