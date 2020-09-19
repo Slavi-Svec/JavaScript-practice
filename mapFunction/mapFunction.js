17a6ed71852b632f735e949a81b968fcca07a971
izaak svec <slavi.svec@gmail.com>
2020-09-19 21:09:22 +1000

    update map function

---
+++ mapFunction/mapFunction.js
@@ -0,0 +1,21 @@
+const array = [1, 2, 3, 4]
+// const arrayForQuestionFour = ['zero', 'one', 'two']
+
+let newArr = []
+for (let i = 0; i < array.length; i++) {
+  newArr.push(array[i] * 3)
+}
+console.log(newArr)
+
+
+const timesThree = array.map(num => num * 3)
+console.log(timesThree)
+
+
+
+//   1) Should log 'zero', 'one', 'two'
+// 2) Should log the above and also the index (0, 1, 2)
+// 3) Should log both of the above plus the whole array (['zero', 'one', 'two']) each time too
+// 4) Should write a map that returns a new array with each numbers times by 3.
+// example 1 console.log(num, index)
+// example 2 console.log(num) console.log(index)
