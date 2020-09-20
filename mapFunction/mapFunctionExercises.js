63269050f3f1e564ba10311785d55d320343aff1
izaak svec <slavi.svec@gmail.com>
2020-09-21 08:12:57 +1000

    complete map exercises

---
+++ mapFunction/mapFunctionExercises.js
@@ -0,0 +1,28 @@
+// 1) Make an array of numbers that are doubles of the first array
+
+// function doubleNumbers(arr){
+  // your code here
+// }
+
+// solution
+
+// const doubleNumbers = (arr) =>  newArr = arr.map(add =>  add * 2)
+
+// console.log(doubleNumbers([2, 5, 100])); // [4, 10, 200]
+
+// 2) Take an array of numbers and make them strings
+
+// function stringItUp(arr){
+//   // your code here
+// }
+
+// const stringItUp = (arr) => newString = arr.map(x => x.toString())
+
+
+// console.log(stringItUp([2, 5, 100])); // ["2", "5", "100"]
+
+function capitalizeNames(arr){
+  // your code here
+}
+
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"])); // ["John", "Jacob", "Jingleheimer", "Schmidt"]
