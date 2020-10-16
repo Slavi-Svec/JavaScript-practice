401e3ef9dedb7db857a870dbcedf0dcb61ec056a
izaak svec <slavi.svec@gmail.com>
2020-10-16 22:57:06 +1100

    update movie objects

--- Buttons/Button  color changer/practice.js
+++ Buttons/Button  color changer/practice.js
@@ -78,11 +78,145 @@
 
 
 
-String.prototype.isUpperCase = function() {
-  return String(this) === String(this).toUpperCase() && String(this) !== String(this).toLowerCase();
+// String.prototype.isUpperCase = function() {
+//   return String(this) === String(this).toUpperCase() && String(this) !== String(this).toLowerCase()
+// }
+
+// // console.log(String.prototype)
+// console.log(isUpperCase("D"))
+
+
+// const numberArr = [5, 8, 13,  12, 10, 12, 9, 6, 99]
+
+// Array.prototype.myFindMethod  = function(arr) {
+//   return arr.find(element => element > 10)
+// }
+
+// console.log(Array.prototype.myFindMethod(numberArr))
+// console.log(Array.prototype)
+
+
+
+// const movieRatings = [
+//   {
+//     theTermintator: 8,
+//   },
+//   {
+//     theTermintatorTwo: 9,
+//   },
+//   {
+//     predator: 9,
+//   },
+//   {
+//     lastActionHero: 5,
+//   },
+//   {
+//     trueLies: 8,
+//   },
+//   {
+//     conanTheBarbarian: 9,
+//   },
+// ]
+// const object = [8, 9, 9, 5,8,9]
+
+// const movies = (obj) => {
+//   if (obj >= 9) {
+//     return obj
+//   }
+// }
+
+// console.log(movies(object))
+
+
+
+// var row = [8, 9, 9, 5, 8, 9]
+// var items = row.length; // number of items in your array
+
+// for (v i = 0; i < items; i++) { // iterate trough all the items
+//     var numberInRow = row[i]; // the number with index number i in rows
+//     var isGreaterThanTen = numberInRow >= 9; // true if the number is greater than ten
+
+//     if (isGreaterThanTen) { // will execute if isGreaterThanTen is true
+//         console.log(numberInRow); // print number greater than 10 to console.
+//     }
+// }
+
+
+// let movies = [8, 9, 9, 5, 8, 9]
+
+// for (let i = 0; i < movies.length; i++) {
+//     let numberInArray = movies[i]
+//     let isRatingGreaterThanNine = numberInArray >= 9
+
+//     if (isRatingGreaterThanNine) {
+//       return numberInArray
+//     }
+// }
+
+// let movies = [8, 9, 9, 5, 8, 9]
+// rating = [9]
+
+// const  findGreater = (arr, elem)  => {
+
+//   return arr.filter((item) => {
+//     return item >= elem
+//   })
+// }
+
+// console.log(findGreater(movies, rating))
+
+
+// const  findGreater = (arr, elem)  => {
+//   let newArr = []
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (elem[i] >= arr[i]) {
+//       console.log(i)
+//     } else {
+//      newArr
+//     }
+//   }
+ 
+// }
+
+
+// 
+// rating = [9]
+
+// const  findGreater = (arr, elem)  => {
+// var i = 0;
+
+// while (numArray[i] < 100) {
+//   console.log((numArray[i++]))
+// }
+
+
+// let movies = [8, 9, 9, 5, 8, 9]
+// elem = (9)
+
+// const  findGreater = (arr, elem)  => {
+// var i = 0;
+
+// while (elem >= arr[i]) {
+//      arr[i++]
+// }
+// }
+
+// console.log(findGreater(movies, elem))
+
+
+
+var Array = (1,4,66,123,240,444);
+
+
+const rock = (numArray) => {
+  var i = 0;
+  while (numArray[i] < 10000) {
+    numArray[i++]
+}
+return numArray
+
 }
 
-console.log(String.prototype)
-
-
+console.log(rock(Array))
 
