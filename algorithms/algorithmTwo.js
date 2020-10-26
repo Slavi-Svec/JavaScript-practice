e4c318128fc11c66d7c5e6c82a4e45b6e76b8b03
izaak svec <slavi.svec@gmail.com>
2020-10-26 14:53:52 +1100

    complete code wars 6kuy Who likes it?

--- algorithms/algorithmTwo.js
+++ algorithms/algorithmTwo.js
@@ -148,17 +148,17 @@
 
 
 
+// ________________________________________________________________________________________________________________
 
+// codewars 6kyu  multiples of 3 and 5
 
+// const solution = (number) => {
+//   if (number <= 0 ) return 0
 
-
-const solution = (number) => {
-  if (number <= 0 ) return 0
-
-  return [...Array(number).keys()]
-    .filter((num) => num % 3 === 0 || num % 5 === 0)
-    .reduce((a, b) =>  a + b)
-}
+//   return [...Array(number).keys()]
+//     .filter((num) => num % 3 === 0 || num % 5 === 0)
+//     .reduce((a, b) =>  a + b)
+// }
 
 // let arrMinusThreeAndFive = numberArray.filter((item) => !filteredArray.includes(item))
 // let newArr = arrMinusThreeAndFive.filter((num) =>  num % 3 === 0 || num % 5 === 0) // minuses numbers divisible by 3&5 from the returned array without the numbers divisible by 3,5
@@ -166,8 +166,65 @@ const solution = (number) => {
 // return addedNumbers
 // \
 
-console.log(solution(10))
+// console.log(solution(10))
 
 
 
 // ________________________________________________________________________________________________________________
+
+// codewars 6 kyu Who likes it?
+
+//  [1] first step i need to check the array if it has the certain amount of indexes in the array
+//  [2] I then need to to a control flow such as an if statemtn or something like that
+
+// const likes  = (names) => {
+//   if (names[0] < names)
+//   return `${names} likes this`
+// }
+
+// console.log((likes(['peter'])))
+
+
+
+// const likes = (array) => {
+//     if (array && !array.length) {
+//       return "no one likes this"
+//   } else if (array.length >= 0 && array.length <= 1) {
+//     const oneArray = array.splice(0,1)
+//       return  `${oneArray} likes this`
+//   } else if  (array.length > 1 && array.length <= 2) {
+//     const twoArray = array.splice(0,2)
+//       return `${twoArray[0]} and ${twoArray[1]} like this`
+//   } else if  (array.length > 2 && array.length <= 3) {
+//     const threeArray = array.splice(0,3)
+//       return `${threeArray[0]}, ${threeArray[1]} and ${threeArray[2]} like this`
+//   } else  if (array.length > 3) {
+//     const numberCount = array.length - 2
+//     const fourArray = array.splice(0,4)
+//       return `${fourArray[0]}, ${fourArray[1]} and ${numberCount} others like this`
+//   }
+// }
+
+// console.log(likes(trumpArray))
+
+const trumpArray = ['trump', 'putin', 'truman']
+
+
+const likes = (array) => {
+  if (array && !array.length) {
+    return "no one likes this"
+} else if (array.length === 1) {
+    return `${array[0]} likes this`
+} else if  (array.length === 2) {
+    return `${array[0]} and ${array[1]} like this`
+} else if  (array.length === 3) {
+    return `${array[0]}, ${array[1]} and ${array[2]} like this`
+} else  if (array.length > 3) {
+    const numberCount = array.length - 2
+    return `${array[0]}, ${array[1]} and ${numberCount} others like this`
+}
+}
+
+
+console.log(likes(trumpArray))
+
