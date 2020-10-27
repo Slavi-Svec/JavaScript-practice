19f6af03080706c3ed1332f690ab80376ece8865
izaak svec <slavi.svec@gmail.com>
2020-10-28 09:51:23 +1100

    started codewars counting Duplicates

--- algorithms/algorithmTwo.js
+++ algorithms/algorithmTwo.js
@@ -331,13 +331,26 @@
 
 // ________________________________________________________________________________________________________________
 
+// Counting Duplicates
+
 
 const duplicateCount = (text) => {
-  let c = text.match([a-zA-Z]).*(\1)
-  return c
+    const filteredOutDuplicated = text.toLowerCase().split('').filter((item, pos, self) =>  self.indexOf(item) !== pos)
+    const finalFilter = filteredOutDuplicated.filter((item, pos, self) =>  self.indexOf(item) == pos)
+    return (finalFilter.length)
 }
 
-console.log(duplicateCount('Indivisibilities'))
+
+
+console.log(duplicateCount("aabBcde"))
+
+
+Test.assertEquals(duplicateCount(""), 0);
+Test.assertEquals(duplicateCount("abcde"), 0);
+Test.assertEquals(duplicateCount("aabbcde"), 2);
+Test.assertEquals(duplicateCount("aabBcde"), 2,"should ignore case");
+Test.assertEquals(duplicateCount("Indivisibility"), 1)
+Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
 
 
 
