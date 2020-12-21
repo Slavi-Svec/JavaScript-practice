7f2320c2f5f44aaed33145ebd268759347dd7a92
izaak svec <slavi.svec@gmail.com>
2020-12-21 23:06:13 +1000

    update code wars

---
+++ codewars/6kyuFindTheParityOutlier.js
@@ -0,0 +1,18 @@
+// code wars Find The Parity Outlier
+
+// first filter out the array to check all the odds
+
+// const findOutlier = (integers) => {
+//   const returnEven = integers.filter((n) => n%2 !==1)
+//   // const returnOdd = integers.filter((n) => n%2 ==1)
+
+//   // return odd
+//   return returnEven
+// }
+
+
+
+// cnsole.log(findOutlier([2,6,8,10,3]))
+// console.log(findOutlier([1, 2, 3]))
+
+
